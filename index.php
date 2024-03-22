6a221fd0c23a99e981fc9532e2dbe99cf183847d
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-03-22 07:54:41 +0300

    add construct to class

--- index.php
+++ index.php
@@ -3,28 +3,32 @@
 class Man
 {
     public $name;
+    public $city;
     public $age;
 
 
-    public function __construct()
+    public function __construct($name, $age)
     {
+        $this->name = $name;
+        $this->age = $age;
     }
 
     public function sayHello()
     {
-        print_r("Hello! My name is " . $this->name . "and I'm " . $this->age . " years old.\n");
+        print_r("Hello! My name is $this->name and I'm $this->age years old.\n");
+        // print_r("Hello! My name is " . $this->name . "and I'm " . $this->age . " years old.\n");
     }
 }
 
 
-$fist_man = new Man();
-$fist_man->name = 'Ivan';
-$fist_man->age = 30;
+$fist_man = new Man('Ivan', 30);
+// $fist_man->name = 'Ivan';
+// $fist_man->age = 30;
 $fist_man->sayHello();
 
-$second_man = new Man();
-$second_man->name = 'Andrey';
-$second_man->age = 25;
+$second_man = new Man('Andrey', 25);
+// $second_man->name = 'Andrey';
+// $second_man->age = 25;
 $second_man->sayHello();
 
 
