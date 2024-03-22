9d8b903311b6c07cbbcdbc3d34b886f1eaaa2b3a
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-03-22 08:59:28 +0300

    add private vars getters and setters

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 
 class Man
 {
-    public $name;
+    private $name;
+    private $birthday;
     public $city;
     public $age;
 
@@ -11,6 +12,26 @@ class Man
     {
         $this->name = $name;
         $this->age = $age;
+    }
+
+    public function setBirthday($year)
+    {
+        $this->birthday = $year;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function getBirthday()
+    {
+        return $this->birthday;
     }
 
     public function sayHello()
@@ -23,11 +44,18 @@ class Man
 
 $fist_man = new Man('Ivan', 30);
 // $fist_man->name = 'Ivan';
-// $fist_man->age = 30;
+$fist_man->age = 35;
+print_r($fist_man->getName() . "\n");
 $fist_man->sayHello();
+$fist_man->setBirthday(1970);
+
+print_r("First man name is " . $fist_man->getName() . "\n");
+print_r("First man age is " . $fist_man->getAge() . "\n");
+print_r("First man birthday year is " . $fist_man->getBirthday() . "\n");
+
 
 $second_man = new Man('Andrey', 25);
-// $second_man->name = 'Andrey';
+// $second_man->name = 'Sergey';
 // $second_man->age = 25;
 $second_man->sayHello();
 
